b8c0ae2b97496104f60f8395cfcc83b8d18b9547
support <support@jspreadsheet.com>
2022-12-29 20:12:43 +0000

    LemonadeJS 3

--- packages/forms/dist/index.js
+++ packages/forms/dist/index.js
@@ -88,6 +88,28 @@
             })
         }
 
+        // Load remote information
+        Component.save = function (url, callback) {
+            var options = {
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors', // no-cors, *cors, same-origin
+                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: 'same-origin', // include, *same-origin, omit
+                headers: {
+                    //'Content-Type': 'application/json',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'X-Requested-With': 'http'
+                },
+                redirect: 'follow', // manual, *follow, error
+                referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                body: JSON.stringify(Component.get()) // body data type must match "Content-Type" header
+            }
+
+            fetch(url, options).then(function (result) {
+                result.json().then(callback)
+            })
+        }
+
         // Get form elements
         Component.get = function () {
             var data = {};
