9ba1e00acb45b9e04991a907f84d0e49069b9af1
support <support@jspreadsheet.com>
2023-01-08 18:52:45 +0000

    V3 documentation

--- packages/create/dist/index.js
+++ packages/create/dist/index.js
@@ -1,8 +1,11 @@
 #! /usr/bin/env node
 
-const process = require('process');
+'use strict';
+
+const admZip = require('adm-zip');
+const request = require('superagent');
 const fs = require('fs');
-const clone = require('git-clone');
+const exec = require('child_process');
 
 const valid = function(data) {
     var pattern = new RegExp(/^[a-zA-Z0-9\_\-\.\s+]+$/);
@@ -16,18 +19,29 @@ const error = function(error) {
 
 const args = process.argv.slice(2);
 
+const repoName = 'base';
+const href = `https://github.com/lemonadejs/${repoName}/archive`;
+const zipFile = 'main.zip';
+const source = `${href}/${zipFile}`;
+const extractEntryTo = `${repoName}-main/`;
+const out = "./" + (args[0] || 'myApp');
+
 if (! args[0] || ! valid(args[0])) {
     error('Project name is not a valid name');
 } else if (fs.existsSync(args[0])) {
     error('You cannot create a new project to an existing folder');
 } else {
-    // Create folder
-    fs.mkdirSync(args[0]);
-    // Go inside the folder
-    process.chdir(args[0]);
-    // Clone repository
-    clone('https://github.com/lemonadejs/base.git', '.', [], function() {
-        console.log('Done!');
-        process.exit(0);
+    request.get(source).on('error', function(error) {
+        console.log(error);
+    }).pipe(fs.createWriteStream(zipFile)).on('finish', function() {
+        var zip = new admZip(zipFile);
+        zip.extractEntryTo(extractEntryTo, './', true);
+        fs.unlink('main.zip', function() {});
+        fs.rename('./' + extractEntryTo, out, function() {
+            process.chdir(out);
+            exec.execSync(`npm i`);
+            // Close process
+            process.exit(0);
+        });
     });
 }
