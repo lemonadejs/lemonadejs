aa6e58ce3afce1f997dfb12f9d85e90ba4012884
support <support@jspreadsheet.com>
2023-01-18 18:00:33 +0000

    LemonadeJS base template

--- packages/template/template/webpack.config.js
+++ packages/template/template/webpack.config.js
@@ -1,39 +1,84 @@
 const path = require('path');
 
-module.exports = {
-    target: 'web',
-    entry: './src/index.js',
-    mode: 'development',
-    optimization: {
-        minimize: false
-    },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'public/dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlReplaceWebpackPlugin = require("html-replace-webpack-plugin");
+
+module.exports = (env, argv) => {
+    const config = {
+        target: 'web',
+        entry: './src/index.js',
+        optimization: {
+            minimize: false
+        },
+        output: {
+            filename: 'index.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        argv.mode === "production"
+                            ? MiniCssExtractPlugin.loader
+                            : "style-loader",
+                        "css-loader",
+                    ],
+                },
+            ],
+        },
+        devServer: {
+            // contentBase
+            static : {
+                directory : path.join(__dirname, "/public")
             },
-        ],
-    },
-    devServer: {
-        // contentBase
-        static : {
-            directory : path.join(__dirname, "/")
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+                "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+            },
+            port: 3005,
+            devMiddleware: {
+                publicPath: "https://localhost:3005/",
+            },
+            hot: "only",
         },
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-        },
-        port: 3005,
-        devMiddleware: {
-            publicPath: "https://localhost:3005/dist/",
-        },
-        hot: "only",
-    },
-    stats: { warnings:false },
+        plugins: [],
+        stats: { warnings:false },
+    };
+
+    if (argv.mode === "production") {
+        config.plugins.push(
+            new MiniCssExtractPlugin({
+                filename: "[contenthash].css",
+            }),
+            new CopyPlugin({
+                patterns: [
+                    {
+                        context: "public",
+                        from: "**/*",
+                        globOptions: {
+                            ignore: ["**/*.html"],
+                        },
+                        noErrorOnMissing: true,
+                    },
+                ],
+            }),
+            new HtmlWebpackPlugin({
+                template: "public/index.html",
+                minify: true,
+                inject: "body",
+            }),
+            new HtmlReplaceWebpackPlugin([
+                {
+                    pattern: '<script src="index.js"></script>',
+                    replacement: "",
+                },
+            ])
+        );
+    }
+
+    return config;
 };
