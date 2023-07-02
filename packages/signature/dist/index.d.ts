5edf99bf7581c8a0538bf5bde7dd8eeff02b7236
support <support@jspreadsheet.com>
2023-07-02 12:38:21 +0100

    Signature pad

---
+++ packages/signature/dist/index.d.ts
@@ -0,0 +1,29 @@
+/**
+ * Official Type definitions for JSS edition bar
+ * https://lemonadejs.net
+ * Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+ */
+
+interface Signature {
+    (): any
+    [key: string]: any
+}
+
+interface options {
+    name?: string;
+    line?: number;
+    value?: number[][];
+    width?: number;
+    height?: number;
+    instructions?: string
+    onchange?: (instance: object) => void;
+    onload?: (instance: object) => void;
+}
+
+interface instance {
+    getValue: () => number[][];
+    setValue: (value: number[]) => void;
+    getImage: () => string;
+}
+
+export declare function Signature(mixed: string | options, options?: options): instance
--- packages/signature/dist/index.js
+++ packages/signature/dist/index.js
@@ -1,49 +1,47 @@
+if (!lemonade && typeof (require) === 'function') {
+    var lemonade = require('lemonadejs');
+}
+
 ;(function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
     global.Signature = factory();
 }(this, (function () {
 
-    // Load LemondeJS
-    if (typeof(lemonade) == 'undefined') {
-        if (typeof(require) === 'function') {
-            var lemonade = require('lemonadejs');
-        } else if (window.lemonade) {
-            var lemonade = window.lemonade;
-        }
-    }
-
-    return function() {
-        var self = this;
-        var x = null;
-        var y = null;
-        var canvas = null;
-        var ctx = null;
+    function Signature() {
+        let self = this;
+        let x = null;
+        let y = null;
+        let canvas = null;
+        let ctx = null;
 
         // Onchange declared event
-        var c = self.onchange;
-        var update = function() {
+        let c = self.onchange;
+        let o = self.onload;
+        let update = function() {
             if (c) {
-                c();
+                c(self);
             }
         }
 
         /**
          * Make sure the value is an array format
          */
-        var valid = function() {
+        const valid = function() {
             if (! Array.isArray(self.value)) {
                 self.value = [];
             }
         }
 
+        valid();
+
         /**
          * Mark the initial position basd on the mouse event
          * @param e
          */
-        var point = function(e) {
+        const point = function(e) {
             if (e.changedTouches && e.changedTouches[0]) {
-                var rect = e.target.getBoundingClientRect();
+                let rect = e.target.getBoundingClientRect();
                 x = e.changedTouches[0].clientX - rect.x;
                 y = e.changedTouches[0].clientY - rect.y;
             } else {
@@ -54,7 +52,7 @@
             self.value.push([ x, y ]);
         }
 
-        var move = function(i, j) {
+        const move = function(i, j) {
             ctx.beginPath();
             ctx.lineWidth = self.line || 3;
             ctx.lineCap = 'round';
@@ -62,14 +60,14 @@
             ctx.moveTo(i, j);
         }
 
-        var line = function(i, j) {
+        const line = function(i, j) {
             ctx.lineTo(i, j);
             ctx.stroke();
         }
 
-        var draw = function(e) {
+        const draw = function(e) {
             if (x !== null) {
-                if (e.which || e.type == 'touchmove') {
+                if (e.which || e.type === 'touchmove') {
                     if (! self.disabled) {
                         move(x, y);
                         point(e);
@@ -85,14 +83,20 @@
             e.preventDefault();
         }
 
+        self.onload = function() {
+            if (o) {
+                o(self);
+            }
+        }
+
         self.onchange = function() {
             // Reset
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             // Update points
-            var c = JSON.parse(JSON.stringify(self.value));
+            let c = JSON.parse(JSON.stringify(self.value));
             if (c && c.length) {
                 // Position to the initial point
-                var t = c.shift();
+                let t = c.shift();
                 move(t[0], t[1]);
 
                 // Draw points
@@ -118,6 +122,14 @@
             return canvas.toDataURL();
         }
 
+        self.getValue = function() {
+            return self.value;
+        }
+
+        self.setValue = function(v) {
+            self.value = v;
+        }
+
         self.init = function(o) {
             canvas = o;
 
@@ -138,10 +150,19 @@
             o.onmousemove = o.ontouchmove = draw;
         }
 
-        var template = `<><canvas value="{{self.value}}" width="{{self.width}}" height="{{self.height}}" @ready="self.init(this)"></canvas><div>{{self.instructions}}</div></>`;
+        return `<><canvas value="{{self.value}}" width="{{self.width}}" height="{{self.height}}" :ready="self.init(this)"></canvas><div>{{self.instructions}}</div></>`;
+    }
 
-        valid();
 
-        return lemonade.element(template, self);
+    // Register signature component across the application
+    lemonade.setComponents({ Signature: Signature });
+
+    return function (root, options) {
+        if (typeof (root) === 'object') {
+            lemonade.render(Signature, root, options)
+            return options;
+        } else {
+            return Signature.call(this, root)
+        }
     }
 })));
