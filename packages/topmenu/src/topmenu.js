ad209293e2069b2eb5f98394856e5d3e7c2bb3fa
support <support@jspreadsheet.com>
2023-09-22 21:54:48 +0100

    topmenu

--- packages/topmenu/src/topmenu.js
+++ packages/topmenu/src/topmenu.js
@@ -16,61 +16,59 @@ if (!Modal && typeof (require) === 'function') {
         if (this.type === 'line') {
             return `<hr />`;
         } else {
-            return `<div data-icon="{{self.icon}}" data-submenu="{{!!self.submenu}}" onmouseover="self.parent.mouseover(self, this);"><a>{{self.title}}</a> <span>{{self.shortcut}}</span></div>`;
+            return `<div data-icon="{{self.icon}}" data-submenu="{{!!self.submenu}}"><a>{{self.title}}</a> <span>{{self.shortcut}}</span></div>`;
         }
     }
 
     const Items = function() {
         let self = this;
 
-        self.mouseover = function(s, element) {
-            if (s.submenu) {
-                self.modalSubmenu.closed = false;
-                self.modalSubmenu.top = element.offsetTop;
-                self.modalSubmenu.left = 250;
-
-                if (! self.modalSubmenu.el.innerText) {
-                    lemonade.render(Items, self.modalSubmenu.el, {options: s.submenu});
-                } else {
-                    self.modalSubmenu.options = s.submenu;
-                }
-            }
-        }
-
-        let template = `<div>
-            <div class="lm-topmenu-menu" >
-            <Item :loop="self.options" />
-            </div>
-            <Modal :closed="true" :ref="self.modalSubmenu" />
-        </div>`;
+        let template = `<div class="lm-topmenu-menu" ><Item :loop="self.options" /></div>`;
         
         return lemonade.element(template, self, { Item: Item });
+    }
+
+    const Create = function() {
+        let self = this;
+
+        let template = `<Modal :closed="true"><Items :ref="self.items" /></Modal>`;
+
+        return lemonade.element(template, self, { Items: Items });
     }
 
     const Topmenu = function() {
         let self = this;
 
-        self.open = function(s, element, e) {
-            if (e.type === 'click') {
-                self.modal.closed = !self.modal.closed;
-            }
-            if (self.modal.closed === false) {
-                self.modal.left = element.offsetLeft;
-                self.modal.items.options = s.submenu;
+
+        self.open = function(e) {
+
+            let index = e.target.getAttribute('data-index');
+
+            if (typeof(index) !== 'undefined') {
+                if (!self.modals.children[index]) {
+                    lemonade.render(Create, self.modals);
+                }
+
+                let modal = self.modals.children[index].self;
+
+                if (e.type === 'click') {
+                    modal.closed = !modal.closed;
+                }
+                if (modal.closed === false) {
+                    modal.left = e.target.offsetLeft;
+                    modal.items.options = e.target.self.submenu;
+                }
             }
         }
 
         return `<div class="lm-topmenu">
-            <div  class="lm-topmenu-options" :loop="self.options">
-                <div class="lm-topmenu-title"
-                    onmouseover="self.parent.open(self, this, e)"
-                    onclick="self.parent.open(self, this, e)">{{self.title}}</div>
+            <div class="lm-topmenu-options" :loop="self.options">
+                <div class="lm-topmenu-title">{{self.title}}</div>
             </div>
-            <Modal :closed="true" :ref="self.modal"><Items :ref="self.items" :options="[]" /></Modal>
         </div>`
     }
 
-    lemonade.setComponents({ Topmenu: Topmenu, Items: Items });
+    lemonade.setComponents({ Topmenu: Topmenu });
 
     return function (root, options) {
         if (typeof (root) === 'object') {
