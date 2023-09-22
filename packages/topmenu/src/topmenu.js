1f0785c6d6248dbe49927605f7ab5971fba46c59
support <support@jspreadsheet.com>
2023-09-22 22:55:38 +0100

    topmenu

--- packages/topmenu/src/topmenu.js
+++ packages/topmenu/src/topmenu.js
@@ -16,55 +16,102 @@ if (!Modal && typeof (require) === 'function') {
         if (this.type === 'line') {
             return `<hr />`;
         } else {
-            return `<div data-icon="{{self.icon}}" data-submenu="{{!!self.submenu}}"><a>{{self.title}}</a> <span>{{self.shortcut}}</span></div>`;
+            return `<div data-icon="{{self.icon}}" data-submenu="{{!!self.submenu}}" onmouseover="self.parent.parent.open(e, self)">
+                <a>{{self.title}}</a> <span>{{self.shortcut}}</span>
+            </div>`;
         }
     }
 
-    const Items = function() {
-        let self = this;
-
-        let template = `<div class="lm-topmenu-menu" ><Item :loop="self.options" /></div>`;
-        
-        return lemonade.element(template, self, { Item: Item });
-    }
+    // Level
+    let index = 0;
 
     const Create = function() {
         let self = this;
 
-        let template = `<Modal :closed="true"><Items :ref="self.items" /></Modal>`;
+        self.open = function(e, s) {
+            if (s.submenu) {
+                let item = self.parent.modals[self.index + 1];
+                if (! item) {
+                    item = self.parent.create();
+                }
+                let parent = self.parent.modals[self.index].modal;
+                let modal = item.modal;
+                modal.closed = false;
+                if (modal.options !== s.submenu) {
+                    modal.top = parent.top + e.target.offsetTop;
+                    modal.left = parent.left + 250;
+                    modal.options = s.submenu;
+                    // Close modals with higher level
+                    self.parent.close(self.index);
+                }
+            } else {
+                // Close modals with higher level
+                self.parent.close(self.index);
+            }
+        }
 
-        return lemonade.element(template, self, { Items: Items });
+        let template = `<Modal :closed="true" :ref="self.modal">
+            <div class="lm-topmenu-menu">
+                <Item :loop="self.options" />
+            </div>
+        </Modal>`;
+
+        return lemonade.element(template, self, { Item: Item });
     }
 
     const Topmenu = function() {
         let self = this;
 
+        // Container for all modals
+        self.modals = [];
 
-        self.open = function(e) {
+        self.create = function() {
+            // Create a new self for each modal
+            let s = {
+                index: index++,
+                parent: self,
+            };
+            // Render the modal inside the main container
+            lemonade.render(Create, self.el.children[1], s);
+            // Add the reference of the modal in a container
+            self.modals.push(s);
+            // Return self
+            return s;
+        }
 
-            let index = e.target.getAttribute('data-index');
+        self.open = function(e, s) {
+            let modal = self.modals[0].modal;
 
-            if (typeof(index) !== 'undefined') {
-                if (!self.modals.children[index]) {
-                    lemonade.render(Create, self.modals);
-                }
-
-                let modal = self.modals.children[index].self;
-
-                if (e.type === 'click') {
-                    modal.closed = !modal.closed;
-                }
-                if (modal.closed === false) {
-                    modal.left = e.target.offsetLeft;
-                    modal.items.options = e.target.self.submenu;
-                }
+            // Click on the top level menu toggle the state of the menu
+            if (e.type === 'click') {
+                modal.closed = !modal.closed;
             }
+
+            // If the modal is open and the content is different from what is shown
+            if (modal.closed === false && modal.options !== s.submenu) {
+                // Define new position
+                modal.top = e.target.offsetTop + e.target.offsetHeight;
+                modal.left = e.target.offsetLeft;
+                // Refresh content
+                modal.options = s.submenu;
+                // Close modals with higher level
+                self.close(0);
+            }
+        }
+
+        self.close = function(level) {
+            self.modals.forEach(function(value, k) {
+                if (k > level) {
+                    value.modal.closed = true;
+                }
+            })
         }
 
         return `<div class="lm-topmenu">
             <div class="lm-topmenu-options" :loop="self.options">
-                <div class="lm-topmenu-title">{{self.title}}</div>
+                <div class="lm-topmenu-title" onmouseover="self.parent.open(e, self)" onclick="self.parent.open(e, self)">{{self.title}}</div>
             </div>
+            <div :ready="self.create()"></div>
         </div>`
     }
 
