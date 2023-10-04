7d35a0f39ad02943b1cb3205f7ec9f1b4e1e0351
support <support@jspreadsheet.com>
2023-10-04 23:00:21 +0100

    Color picker updates. New inline mode.

--- packages/color/src/index.js
+++
@@ -1,8 +0,0 @@
-import Color from './color.js';
-import './style.css';
-import '@lemonadejs/modal/dist/style.css'
-import '@lemonadejs/tabs/dist/style.css'
-
-window.Color = Color;
-
-export default Color;
